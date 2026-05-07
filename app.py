f436db8be78e28a700565e05a18a4c62a81a2344
Ubuntu <ubuntu@ip-172-31-34-75.ap-south-1.compute.internal>
2026-05-07 16:48:26 +0000

    Webhook test

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Webhook Trigger Successful 🚀")")
+print("Webhook Trigger Successful 🚀")
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "CI/CD Pipeline Working 🚀"
+
+app.run(host='0.0.0.0', port=5000)
